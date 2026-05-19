341b113a1be7b1792995e8e3cf07c327e641aa9d
Jeny Sherlyn Raj <jenysherlynraj2005@gmail.com>
2026-05-19 12:29:17 +0530

    Add User model with validation attributes

---
+++ User.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Range(18, 100)]
+        public int Age { get; set; }
+    }
+}using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Range(18, 100)]
+        public int Age { get; set; }
+    }
+}
